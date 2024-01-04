92f0def034ef2afa7f9869c87ec86d3166bfbf0f
Alejandro Morales Rios <legorios@gmail.com>
2024-01-03 23:07:48 -0500

    Add color classes for red, yellow, and green backgrounds and text

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,12 @@ const colorClasses = [
   'text-orange-800', 
 	'bg-blue-100', 
   'text-blue-800', 
+	'bg-red-100', 
+  'text-red-800', 
+	'bg-yellow-100', 
+  'text-yellow-800', 
+	'bg-green-100', 
+  'text-green-800', 
 ];
 
 
